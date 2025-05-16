7ec91eb63346893a982ebed449bed7ca595d80cd
Barbod Habibi <barbod@kpmpower.com>
2025-05-16 09:06:46 -0400

    Set up API call with Vite proxy to resolve CORS issue

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,34 @@ import Map from "./components/Map/Map.tsx";
 import OutputForm from "./components/OutputForm/OutputForm.tsx";
 import { useState } from "react";
 import { MapRendererProps } from "./components/Map/Map.types";
+import { api } from "./data/api.ts";
 
 const App = () => {
-  // Hooks
-  const [mapConfig, setMapConfig] = useState<MapRendererProps["mapConfig"]>({
-    minBuildings: 0,
-    thresholdHigh: 90,
-    thresholdMedium: 80,
-    colorHigh: "#008000",
-    colorMedium: "#FFA500",
-    colorLow: "#FF0000",
-    displayMode: "individual",
-  });
+    const fetchData = async () => {
+        const data = await api.get("/package_show", { params: { id: "dinesafe" } });
+        console.log(data);
+        return data;
+    }
+    fetchData();
 
-  return (
-    <>
-      <InputForm mapConfig={mapConfig} setMapConfig={setMapConfig} />
-      <Map mapConfig={mapConfig} />
-      <OutputForm />
-    </>
-  );
+    // Hooks
+    const [mapConfig, setMapConfig] = useState<MapRendererProps["mapConfig"]>({
+        minBuildings: 0,
+        thresholdHigh: 90,
+        thresholdMedium: 80,
+        colorHigh: "#008000",
+        colorMedium: "#FFA500",
+        colorLow: "#FF0000",
+        displayMode: "individual",
+    });
+
+    return (
+        <>
+            <InputForm mapConfig={mapConfig} setMapConfig={setMapConfig} />
+            <Map mapConfig={mapConfig} />
+            <OutputForm />
+        </>
+    );
 };
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/ckan-api": {
+        target: "https://ckan0.cf.opendata.inter.prod-toronto.ca",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ckan-api/, ""),
+      },
+    },
+  },
 })
